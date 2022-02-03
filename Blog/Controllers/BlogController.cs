3584b884774c3cc1282e4c600a90f6e8958a128b
Kyle Givler <xvegan88x@gmail.com>
2022-02-03 18:14:30 -0500

    API Example

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCBlog.Data;
+using MVCBlog.DTOs;
+using MVCBlog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MVCBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IEnumerable<BlogDTO>> GetBlogs()
+        {
+            var blogs = new List<BlogDTO>();
+            var blogModels = _context.Blogs;
+
+            foreach(var b in blogModels)
+            {
+                blogs.Add(new BlogDTO
+                {
+                    Id = b.Id,
+                    AuthorId = b.AuthorId,
+                    Name = b.Name,
+                    Description = b.Description,
+                    Created = b.Created,
+                    Updated = b.Updated,
+                });
+            }
+            return blogs;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBlog(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            if(blog == null)
+            {
+                return NotFound();
+            }
+
+            var dto = new BlogDTO
+            {
+                Id = blog.Id,
+                AuthorId = blog.AuthorId,
+                Name = blog.Name,
+                Description = blog.Description,
+                Created = blog.Created,
+                Updated = blog.Updated,
+            };
+
+            return Ok(dto);
+        }
+    }
+}
---
+++ Blog/DTOs/BlogDTO.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using MVCBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCBlog.DTOs
+{
+    public class BlogDTO
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+    }
+}
